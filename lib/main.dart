8db8303e420bbbd7541062962e415f37a3e86a6c
rui-shimazaki <rui-shimazaki@cuebic.co.jp>
2022-03-31 21:28:13 +0900

    [UPD]Riverpod導入→使用までできたので記録

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,12 @@ import 'package:daypay/screens/signup_screen.dart';
 import 'package:daypay/screens/home_screen.dart';
 import 'package:daypay/utils/colors.dart';
 import 'package:daypay/widgets/text_field_input.dart';
+
+import 'package:flutter/material.dart';
+
+// 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
+// プロバイダを使うことで値のモックやオーバーライドが可能になります。
+final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,14 +35,23 @@ void main() async {
     await Firebase.initializeApp();
   }
   print('start');
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // `ref.watch` を使用して Providerを読み取ります。
+    final String value = ref.watch(helloWorldProvider);
+    // `HookConsumerWidget` を継承しているので `useXxx` メソッドが使用できる。
+    final counter = useState(0);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
@@ -44,7 +61,7 @@ class MyApp extends StatelessWidget {
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
       // ),
-      home: SignupScreen(),
+      home: Text(value),
     );
   }
 }
