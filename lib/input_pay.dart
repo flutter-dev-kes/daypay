a510005ac5362a27bdd69bda711c58502b1f667f
Masaya <prssilversky@gmail.com>
2022-04-03 23:24:37 +0900

    [UPD]使用金額入力画面のUI変更→コンテナをボタン化し、カラー等を変更

--- lib/input_pay.dart
+++ lib/input_pay.dart
@@ -2,6 +2,7 @@ import 'package:daypay/check_monthly_using.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'check_daily_using.dart';
+import 'package:flutter/src/material/icons.dart';
 
 class InputPay extends StatelessWidget {
   final valueController = TextEditingController();
@@ -11,52 +12,67 @@ class InputPay extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('DayPay'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '使用金額',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-            ),
-            TextField(
-              controller: valueController,
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly // ③ 数字入力のみ許可する
-              ],
-              autofocus: true,
-            ),
-            ElevatedButton(
-              child: const Text('今日使った金額へ遷移する'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(15),
-                primary: Colors.orange,
-                onPrimary: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            CheckDailyUsing(valueController.text)));
-              },
-            ),
-            ElevatedButton(
-              child: Text('カレンダーへ'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(15),
-                primary: Colors.teal,
-                onPrimary: Colors.white,
-              ),
+        actions: [
+          IconButton(
               onPressed: () {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => CheckMonthlyUsing()));
               },
-            )
+              icon: Icon(Icons.calendar_month))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                padding: EdgeInsets.fromLTRB(40, 50, 40, 50),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.teal,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '使用金額',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Color.fromARGB(255, 240, 229, 146),
+                      ),
+                    ),
+                    TextField(
+                      controller: valueController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly // ③ 数字入力のみ許可する
+                      ],
+                      autofocus: true,
+                    )
+                  ],
+                )),
+            InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              CheckDailyUsing(valueController.text)));
+                },
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 175, 231, 226),
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Icon(Icons.currency_yen), Text('submit')]),
+                )),
           ],
         ),
       ),
