79fb8ff2f823eac356607873f04adda66501b67b
rui-shimazaki <rui-shimazaki@cuebic.co.jp>
2022-04-04 23:36:46 +0900

    [UPD]Riverpod使用のための諸修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:daypay/test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'package:daypay/responsive/mobile_screen_layout.dart';
 import 'package:daypay/responsive/responsive_layout_screen.dart';
@@ -28,17 +30,21 @@ void main() async {
     await Firebase.initializeApp();
   }
   print('start');
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
+      title: 'daypay',
       // theme: ThemeData.dark()
       //     .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
       // home: const ResposinveLauout(
